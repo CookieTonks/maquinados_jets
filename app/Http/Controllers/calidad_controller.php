30245c9d4653e72c4813799b3e9521a73aa766a7
CookieTonks <91743424+CookieTonks@users.noreply.github.com>
2023-03-03 13:36:47 -0600

    Plantilla JETS

---
+++ app/Http/Controllers/calidad_controller.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models;
+use App\Models\inspections;
+use App\Models\jets_rutas;
+use App\Models\orders;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use PhpParser\Node\Stmt\Echo_;
+use Illuminate\Support\Facades\Storage;
+
+
+class calidad_controller extends Controller
+{
+    public function dashboard_calidad()
+    {
+        $notificaciones =  Models\notifications::all();
+
+        $ordenes = DB::table('orders')
+            ->join('jets_rutas', 'orders.id', '=', 'jets_rutas.ot')
+            ->where('jets_rutas.sistema_produccion', '=', 'DONE')
+            ->where('jets_rutas.sistema_calidad', '=', '-')
+            ->select('orders.*')
+            ->get();
+
+
+
+        return view('modulos.calidad.dashboard_calidad', compact('ordenes', 'notificaciones'));
+    }
+
+    public function buscador_calidad()
+    {
+        $ordenes = DB::table('orders')
+            ->join('jets_rutas', 'orders.id', '=', 'jets_rutas.ot')
+            ->where('jets_rutas.sistema_produccion', '=', 'DONE')
+            ->where('jets_rutas.sistema_calidad', '=', '-')
+            ->select('orders.*')
+            ->get();
+
+            $notificaciones =  Models\notifications::all();
+
+
+
+        return view('modulos.calidad.buscador_calidad', compact('ordenes', 'notificaciones'));
+    }
+
+    public function calidad_embarques(Request $request)
+    {
+
+
+        if ($request->tipo_inspeccion === 'LIBERADO') {
+
+            $rutas_jets = models\jets_rutas::where('ot', '=', $request->ot)->first();
+            $rutas_jets->sistema_calidad = 'DONE';
+            $rutas_jets->save();
+
+            $registro_jets = new models\jets_registros();
+            $registro_jets->ot = $rutas_jets->ot;
+            $registro_jets->movimiento = 'CALIDAD - EMBARQUES';
+            $registro_jets->responsable = Auth::user()->name;
+            $registro_jets->save();
+            
+             Storage::disk('public')->putFileAs('liberacion/' . $request->ot, $request->file('doc'), $request->ot . '.pdf');
+        } else if ($request->tipo_inspeccion === 'RETRABAJO') {
+
+            $inspections = new models\inspections();
+            $inspections->ot = $request->ot;
+            $inspections->tipo_inspeccion = $request->tipo_inspeccion;
+            $inspections->cant_scrap = $request->cant_scrap;
+            $inspections->cant_liberada = $request->cant_liberada;
+            $inspections->cant_retrabajo = $request->cant_retrabajo;
+            $inspections->usuario =  Auth::user()->name;
+            $inspections->observaciones = $request->observaciones;
+            $inspections->save();
+
+            $reporte = new models\pconforme();
+            $reporte->ot = $request->ot;
+            $reporte->num_parte = $request->num_parte;
+            $reporte->operador = $request->operador;
+            $reporte->descripcion = $request->descripcion;
+            $reporte->cant = $request->cant_scrap;
+            $reporte->cliente = $request->cliente;
+            $reporte->analisis = $request->analisis;
+            $reporte->origen = $request->origen;
+            $reporte->disposicion = $request->disposicion;
+            $reporte->save();
+
+            $rutas_jets = models\jets_rutas::where('ot', '=', $request->ot)->first();
+            $rutas_jets->sistema_produccion = '-';
+            $rutas_jets->save();
+
+            $produccion = models\production::where('ot', '=', $request->ot)->first();
+            $produccion->estatus = 'RETRABAJO';  
+            $produccion->save();
+
+            $registro_jets = new models\jets_registros();
+            $registro_jets->ot = $rutas_jets->ot;
+            $registro_jets->movimiento = 'CALIDAD - RETRABAJO';
+            $registro_jets->responsable = Auth::user()->name;
+            $registro_jets->save();
+            
+              Storage::disk('public')->putFileAs('retrabajo/' . $request->ot, $request->file('doc'), $request->ot . '.pdf');
+
+        } else if ($request->tipo_inspeccion === 'SCRAP') {
+
+            $inspections = new models\inspections();
+            $inspections->ot = $request->ot;
+            $inspections->tipo_inspeccion = $request->tipo_inspeccion;
+            $inspections->cant_scrap = $request->cant_scrap;
+            $inspections->cant_liberada = $request->cant_liberada;
+            $inspections->cant_retrabajo = $request->cant_retrabajo;
+            $inspections->usuario =  Auth::user()->name;
+            $inspections->observaciones = $request->observaciones;
+            $inspections->save();
+
+            $reporte = new models\pconforme();
+            $reporte->ot = $request->ot;
+            $reporte->num_parte = $request->num_parte;
+            $reporte->operador = $request->operador;
+            $reporte->descripcion = $request->descripcion;
+            $reporte->cant = $request->cant_scrap;
+            $reporte->cliente = $request->cliente;
+            $reporte->analisis = $request->analisis;
+            $reporte->origen = $request->origen;
+            $reporte->disposicion = $request->disposicion;
+            $reporte->save();
+
+            $rutas_jets = models\jets_rutas::where('ot', '=', $request->ot)->first();
+            $rutas_jets->sistema_produccion = '-';
+            $rutas_jets->sistema_almacenr = '-';
+            $rutas_jets->sistema_compras = '-';
+            $rutas_jets->sistema_almacen = '-';
+            $rutas_jets->save();
+
+            $produccion = models\production::where('ot', '=', $request->ot)->first();
+            $produccion->estatus = 'ASIGNADA';
+            $produccion->save();
+
+            $registro_jets = new models\jets_registros();
+            $registro_jets->ot = $rutas_jets->ot;
+            $registro_jets->movimiento = 'CALIDAD - SCRAP';
+            $registro_jets->responsable = Auth::user()->name;
+            $registro_jets->save();
+
+            $material = models\materiales::where('ot', '=', $request->ot)->get();
+            
+              Storage::disk('public')->putFileAs('scrap/' . $request->ot, $request->file('doc'), $request->ot . '.pdf');
+
+            foreach ($material as $material) {
+                $material->estatus = 'P/ALMACEN';
+                $material->save();
+            }
+
+          
+        }
+        return back()->with('mensaje-success', '¡Inpección realizada con exito!');
+    }
+
+    public function calidad_produccion($orden)
+    {
+        $orden_produccion = models\production::where('ot', '=', $orden)->first();
+        $orden_produccion->estatus = 'L.Produccion';
+        $orden_produccion->save();
+
+        $rutas_jets = models\jets_rutas::where('ot', '=', $orden)->first();
+        $rutas_jets->sistema_produccion = '-';
+        $rutas_jets->save();
+
+        $registro_jets = new models\jets_registros();
+        $registro_jets->ot = $orden_produccion->ot;
+        $registro_jets->movimiento = 'CALIDAD - PRODUCCION';
+        $registro_jets->responsable = Auth::user()->name;
+        $registro_jets->save();
+
+        return back()->with('mensaje-success', '¡OT regresada a producción con éxito!');
+    }
+
+
+    public function rechazo_cliente(Request $request)
+    {
+        $orden = models\production::where('ot', '=', $request->ot)->first();
+        $orden->estatus = 'E.CALIDAD';
+        $orden->save();
+
+        $rutas_jets = models\jets_rutas::where('ot', '=', $request->ot)->first();
+        $rutas_jets->sistema_calidad = '-';
+        $rutas_jets->sistema_produccion = 'DONE';
+
+        $rutas_jets->save();
+
+        return back()->with('mensaje-success', 'Rechazo registrado con exito!');
+    }
+}
---
+++ app/Http/Middleware/programador_middleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class programador_middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->role == 'Administrador') {
+            return $next($request);
+
+            if (Auth::user()->role == 'Supervisor producción') {
+                return $next($request);
+            }
+            if (Auth::user()->role == 'Programador') {
+                return $next($request);
+            } else {
+                return redirect()->route('dashboard');
+            }
+        }
+    }
+}
